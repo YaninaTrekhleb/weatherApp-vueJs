2327b91901d145cc683d03326d8aa02b8b5f16d4
Yanina Trekhleb <yanisavchenko@gmail.com>
2020-08-10 15:37:50 +0200

    Update readme file.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# weatherApp-vueJs
+# Weather App in Vue.Js by Tyler Potts 
+## [Link to the video](https://www.youtube.com/watch?v=JLc-hWsPTUY&t=388s)
+
+[▶️ Launch Demo]()
+
+![Demo](src/assets/../../weather-app/src/assets/demo-weather-app.gif)
--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..."/>
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northhampton, UK</div>
-          <div class="date">Monday 10 August 2020</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temperature">30°</div>
-          <div class="weather">Sun</div>
+          <div class="temperature">{{ Math.round(weather.main.temp)}}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -24,7 +30,34 @@ export default {
   name: 'app',
   data () {
     return {
-      api_key: '666286886c5d52f0bb028c1a10401486'
+      api_key: '666286886c5d52f0bb028c1a10401486',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
@@ -33,9 +66,8 @@ export default {
 
 <style>
   * {
-    margin: 0;
+    margin: 0 auto;
     padding: 0;
-    box-sizing: border-box;
   }
 
   body {
@@ -43,9 +75,6 @@ export default {
   }
 
   #app {
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
     background-image: url('./assets/cold-bg.jpeg');
     background-position: center;
     background-size: cover;
@@ -53,7 +82,14 @@ export default {
     transition: 0.4s;
   }
 
+  #app.warm {
+    background-image: url('./assets/warm-bg.jpeg');
+  }
+
   main {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
     min-height: 100vh;
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.4));
@@ -65,8 +101,10 @@ export default {
   }
 
   .search-box .search-bar {
-    display: block;
-    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
     padding: 15px;
 
     color: #313131;
